332f282fd27d0836eaafcde66b8ed88d19dad1b1
sharkbound <laptopblaster@gmail.com>
2019-02-14 21:00:03 -0800

    added reason and cmd attributes to InvalidArgumentsError

--- twitchbot/exceptions.py
+++ twitchbot/exceptions.py
@@ -1,4 +1,19 @@
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .command import Command
+
+
 class InvalidArgumentsError(Exception):
     """
     used to trigger a command's help message in twitch chat when caller gives the command invalid arguments
     """
+
+    # this is a default for if the class is raised without calling its init, ex: raise InvalidArgumentsError
+    reason: str = 'invalid arguments'
+    cmd: 'Command' = None
+
+    def __init__(self, reason: str = reason, cmd: 'Command' = None):
+        super().__init__(reason)
+        self.cmd: 'Command' = cmd
+        self.reason: str = reason
