c35274d21aaeeef18d8eaef75cf34d53e64bb22c
sharkbound <laptopblaster@gmail.com>
2019-02-19 15:16:38 -0800

    updated fun commands to raise invalidarguementserror

--- twitchbot/builtin_commands/fun_commands.py
+++ twitchbot/builtin_commands/fun_commands.py
@@ -5,26 +5,23 @@ from twitchbot import (
     Message,
     CommandContext,
     Command,
-    InvalidArgumentsError)
+    InvalidArgumentsError
+)
 
 
 @Command('roll', context=CommandContext.BOTH, syntax='(sides)', help='rolls a X sided die')
 async def cmd_roll(msg: Message, *args):
-    if not args:
-        sides = 6
-    else:
-        try:
-            sides = int(args[0])
-        except ValueError:
-            await msg.reply('invalid value for sides')
-            return
+    try:
+        sides = int(args[0]) if args else 6
+    except ValueError:
+        raise InvalidArgumentsError(reason='invalid value for sides', cmd=cmd_roll)
 
     num = randint(1, sides)
     user = msg.mention if msg.is_privmsg else ''
     await msg.reply(f'{user} you rolled a {num}')
 
 
-@Command('crashcode', permission='crashcode', help='¯\_(ツ)_/¯')
+@Command('crashcode', permission='crashcode', help=r'¯\_(ツ)_/¯')
 async def cmd_crash_code(msg: Message, *args):
     await msg.reply(f'you may not crash me! {msg.mention}')
 
@@ -32,7 +29,7 @@ async def cmd_crash_code(msg: Message, *args):
 @Command('choose', syntax='<option> <option> ect', help='chooses a random option passed to the command')
 async def cmd_choose(msg: Message, *args):
     if len(args) < 2:
-        raise InvalidArgumentsError
+        raise InvalidArgumentsError(reason='missing required arguments', cmd=cmd_choose)
 
     await msg.reply(f'result: {choice(args)}')
 
@@ -40,7 +37,7 @@ async def cmd_choose(msg: Message, *args):
 @Command('color', permission='color', syntax='<color>', help='sets the bots chat color')
 async def cmd_color(msg: Message, *args):
     if not args:
-        raise InvalidArgumentsError
+        raise InvalidArgumentsError(reason='missing required arguments', cmd=cmd_color)
 
     await msg.channel.color(args[0])
     await msg.reply(f'set color to {args[0]}')
