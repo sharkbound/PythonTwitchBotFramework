f63db592cde626fae3d9bdb123637fd6f2e25434
sharkbound <laptopblaster@gmail.com>
2019-04-23 16:25:04 -0500

    incremented version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as f:
 
 setup(
     name='PythonTwitchBotFramework',
-    version='1.0.1',
+    version='1.0.3',
     packages=find_packages(),
     url='https://github.com/sharkbound/PythonTwitchBotFramework',
     license='MIT',
