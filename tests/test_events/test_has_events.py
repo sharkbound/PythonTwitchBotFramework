369d9818fa35842fafca9cc6144d5fa4535c8183
sharkbound <laptopblaster@gmail.com>
2019-07-23 17:27:01 -0500

    error string for events unit test

--- tests/test_events/test_has_events.py
+++ tests/test_events/test_has_events.py
@@ -1,11 +1,11 @@
-from twitchbot import BaseBot, Mod, Event, cfg
+from twitchbot import BaseBot, Mod, Event
 
 
 def test_base_bot_events_are_set():
     for event in Event:
-        assert event.name in BaseBot.__dict__
+        assert event.name in BaseBot.__dict__, f'BaseBot must implement event {event}'
 
 
 def test_mod_events_are_set():
     for event in Event:
-        assert event.name in Mod.__dict__
+        assert event.name in Mod.__dict__, f'Mod must implement event {event}'
