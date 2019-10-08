eca2fcbac4bb3feb65c87fa03edf113ef447dbf5
Jake <20249180+netmasa@users.noreply.github.com>
2019-10-08 15:39:06 -0700

    Update modloader.py

--- twitchbot/modloader.py
+++ twitchbot/modloader.py
@@ -112,6 +112,12 @@ class Mod:
         """
         triggered when a bit donation is posted in chat
         """
+    async def on_channel_raided(self, channel: Channel, NumViewers: int):
+        """
+        triggered when the current channel is raided
+        :param channel: the channel who raided
+        :param NumViewers: the number of viewers who joined in the raid
+        """
 
     async def on_channel_joined(self, channel: Channel):
         """
