aeaa155e07d67cb6b850bcf880d4e5c21de044e8
sharkbound <laptopblaster@gmail.com>
2019-10-13 21:59:38 -0500

    fix for when tkinter is not available

--- twitchbot/gui/getpassgui.py
+++ twitchbot/gui/getpassgui.py
@@ -1,5 +1,4 @@
 import webbrowser
-from tkinter import *
 from collections import namedtuple
 
 __all__ = ('show_auth_gui', 'AuthInfo')
@@ -9,23 +8,29 @@ AuthInfo = namedtuple('AuthInfo', 'username oauth')
 
 
 def show_auth_gui():
-    root = Tk()
+    try:
+        import tkinter as tk
+    except ImportError:
+        print('unable to import tkinter to show the GUI!')
+        return
+
+    root = tk.Tk()
     width = 30
 
-    Label(root, text='bot account name:').grid(row=0, column=0)
-    username = StringVar(root)
-    Entry(root, textvar=username, width=width).grid(row=0, column=1)
+    tk.Label(root, text='bot account name:').grid(row=0, column=0)
+    username = tk.StringVar(root)
+    tk.Entry(root, textvar=username, width=width).grid(row=0, column=1)
 
-    Label(root, text='oauth:').grid(row=1, column=0)
-    password = StringVar(root)
-    Entry(root, show='*', textvar=password, width=width).grid(row=1, column=1)
+    tk.Label(root, text='oauth:').grid(row=1, column=0)
+    password = tk.StringVar(root)
+    tk.Entry(root, show='*', textvar=password, width=width).grid(row=1, column=1)
 
-    Button(
+    tk.Button(
         text='open twitch oauth generator website',
         command=lambda: webbrowser.open(OAUTH_GENERATOR_URL)
     ).grid(row=2, column=0)
 
-    Button(root, text='im done', command=lambda: root.destroy()).grid(row=2, column=1)
+    tk.Button(root, text='im done', command=lambda: root.destroy()).grid(row=2, column=1)
 
     root.mainloop()
     return AuthInfo(username=username.get(), oauth=password.get())
