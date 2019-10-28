de85a160c2e5f9552d8248bb61d6fd71511645c9
sharkbound <laptopblaster@gmail.com>
2019-10-27 19:24:20 -0500

    added __all__ to ratelimit.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as f:
 
 setup(
     name='PythonTwitchBotFramework',
-    version='1.6.15',
+    version='1.6.16',
     packages=find_packages(),
     url='https://github.com/sharkbound/PythonTwitchBotFramework',
     license='MIT',
