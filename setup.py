40acea9a3fdb7c247081410919c8f966db859c36
sharkbound <laptopblaster@gmail.com>
2019-11-03 17:56:05 -0600

    inc pypi version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as f:
 
 setup(
     name='PythonTwitchBotFramework',
-    version='1.8.19',
+    version='1.8.20',
     packages=find_packages(),
     url='https://github.com/sharkbound/PythonTwitchBotFramework',
     license='MIT',
