5fceebf93fabbbcbcd5295027ac1a8cd81b58252
RogueDarkJedi <roguedarkjedi@users.noreply.github.com>
2020-05-09 15:19:01 -0700

    Make sql objects multithreaded friendly
    Really quick hack to make the SQL database access and objects more multithread friendly. For most cases, no one will care but if the bot is encapsulated into a thread, this will make it such that someone could recreate the SQL environment on run.
    
    Appears to work properly in tests. Alternatives could be to use scoped_sessions, so long as the engine is properly set up.
    
    The idea here allows someone to destroy the previous database system on the bot's class initialization, and recreate it during the bot's run.

--- twitchbot/database/session.py
+++ twitchbot/database/session.py
@@ -2,7 +2,12 @@ from sqlalchemy import create_engine, orm
 from sqlalchemy.ext.declarative import declarative_base
 from ..config import mysql_cfg
 
-__all__ = ('Base', 'engine', 'session', 'DB_FILENAME', 'init_tables')
+__all__ = ('Base', 'engine', 'session', 'DB_FILENAME', 'init_tables', 'connect_database', 'shutdown_database')
+
+DB_FILENAME = 'database.sqlite'
+Base = declarative_base()
+engine = None
+session: orm.Session = None
 
 if mysql_cfg.enabled:
     try:
@@ -13,14 +18,30 @@ if mysql_cfg.enabled:
         input('\npress enter to exit...')
         exit(1)
 
-Base = declarative_base()
-DB_FILENAME = 'database.sqlite'
-engine = create_engine(f'sqlite:///{DB_FILENAME}'
+def connect_database():
+    global engine
+    global session
+     
+    engine = create_engine(f'sqlite:///{DB_FILENAME}'
                        if not mysql_cfg.enabled else
                        f'mysql+mysqlconnector://{mysql_cfg.username}:{mysql_cfg.password}@{mysql_cfg.address}:{mysql_cfg.port}/{mysql_cfg.database}')
-Session = orm.sessionmaker(bind=engine)
-session: orm.Session = Session()
+    Session = orm.sessionmaker(bind=engine)
+    session = Session()
 
-
+def shutdown_database():
+    global engine
+    global session
+    
+    session.commit()
+    session.close()
+    session = None
+    engine.dispose()
+    engine = None
+    
 def init_tables():
+    if engine is None:
+        return
+    
     Base.metadata.create_all(engine)
+
+connect_database()
