06b72090cf946e91eba0af3ec77023455f8eec5a
sharkbound <laptopblaster@gmail.com>
2020-05-19 15:41:49 -0500

    restrict python install version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as f:
 
 setup(
     name='PythonTwitchBotFramework',
-    version='1.11.7',
+    version='1.11.8',
     python_requires='>=3.6',
     packages=find_packages(),
     url='https://github.com/sharkbound/PythonTwitchBotFramework',
