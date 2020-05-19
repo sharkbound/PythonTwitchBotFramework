26d52c21649e1aaaf1cea6571c203b52d958f825
sharkbound <laptopblaster@gmail.com>
2020-05-18 20:07:02 -0500

    added add_nameless_task(coro: Coroutine)

--- twitchbot/util/task_util.py
+++ twitchbot/util/task_util.py
@@ -1,13 +1,25 @@
-from asyncio import ensure_future, Task, sleep, get_event_loop
-from typing import Dict, Coroutine, Optional
+from asyncio import ensure_future, Task, sleep, get_event_loop, Future
+from typing import Dict, Coroutine, Optional, Tuple
 
-__all__ = ('active_tasks', 'add_task', 'get_task', 'task_running', 'stop_task', 'task_exist', 'stop_all_tasks')
+__all__ = ('active_tasks', 'add_task', 'get_task', 'task_running', 'stop_task', 'task_exist', 'stop_all_tasks', 'add_nameless_task')
 
 active_tasks: Dict[str, Task] = {}
 
 
 def add_task(name: str, coro: Coroutine):
     active_tasks[name.lower()] = ensure_future(coro)
+
+
+nameless_task_counter = 0
+
+
+def add_nameless_task(coro: Coroutine) -> Tuple[str, Future]:
+    global nameless_task_counter
+    nameless_task_name = f'nameless_task_{nameless_task_counter}'
+    nameless_task_counter += 1
+    future = ensure_future(coro)
+    active_tasks[nameless_task_name] = future
+    return nameless_task_name, future
 
 
 def get_task(name: str) -> Optional[Task]:
