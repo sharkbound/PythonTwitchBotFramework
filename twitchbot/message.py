a2861a7227bbdd89f58f041ebf5de8b5b7f5a1e5
sharkbound <laptopblaster@gmail.com>
2020-07-24 13:51:04 -0500

    Revert "added util function `Message._get_channel_or_default` to remove KeyError potential when parsing messages"
    This reverts commit b5fdbd23

--- twitchbot/message.py
+++ twitchbot/message.py
@@ -39,9 +39,6 @@ class Message:
 
         self._parse()
 
-    def _get_channel_or_default(self, channel_name: str, default=None):
-        return channels.get(channel_name, default)
-
     def _normalize(self, s: str):
         return s.strip().casefold()
 
@@ -103,7 +100,7 @@ class Message:
     def _parse_user_part(self) -> bool:
         m = RE_USER_PART.search(self.raw_msg)
         if m:
-            self.channel = self._get_channel_or_default(m.get('channel'))
+            self.channel = channels[m['channel']]
             self.author = m['user']
             self.type = MessageType.USER_PART
 
@@ -138,7 +135,7 @@ class Message:
     def _parse_privmsg(self) -> bool:
         m = RE_PRIVMSG.search(self.raw_msg)
         if m:
-            self.channel = self._get_channel_or_default(m.get('channel'))
+            self.channel = channels[m['channel']]
             self.author = m['user']
             self.content = m['content']
             self.type = MessageType.PRIVMSG
@@ -163,7 +160,7 @@ class Message:
         m = RE_USERNOTICE.search(self.raw_msg)
         if m:
             self.tags = Tags(m['tags'])
-            self.channel = self._get_channel_or_default(m.get('channel'))
+            self.channel = channels[m['channel']]
             self.author = self.tags.all_tags.get('login')
             self.content = m['content']
             if self.tags.msg_id in {'sub', 'resub', 'subgift', 'anonsubgift', 'submysterygift', 'anongiftpaidupgrade',
@@ -186,7 +183,7 @@ class Message:
         m = RE_NOTICE.search(self.raw_msg)
         if m:
             self.tags = Tags(m['tags'])
-            self.channel = self._get_channel_or_default(m.get('channel'))
+            self.channel = channels[m['channel']]
             self.content = m['content']
             if self.tags.msg_id == 'msg_banned':
                 self.type = MessageType.BOT_PERMANENTLY_BANNED
