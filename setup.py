845082b528980f84cd0be6bc3fb5f89627c5b5e6
sharkbound <laptopblaster@gmail.com>
2020-07-29 15:18:55 -0500

    incr version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as f:
 
 setup(
     name='PythonTwitchBotFramework',
-    version='1.13.5',
+    version='1.13.6',
     python_requires='>=3.6',
     packages=find_packages(),
     url='https://github.com/sharkbound/PythonTwitchBotFramework',
