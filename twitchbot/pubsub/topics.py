14df23f232a1b90e8c09d6d72b92885f022009d6
sharkbound <laptopblaster@gmail.com>
2020-07-29 21:55:34 -0500

    removed bits_v1 pubsub topic, replaced it with bits instead and used bitsv2

--- twitchbot/pubsub/topics.py
+++ twitchbot/pubsub/topics.py
@@ -1,6 +1,6 @@
 class PubSubTopics:
-    bits_v1 = 'channel-bits-events-v1.'
-    # bits_v2 = 'channel-bits-events-v2.'
+    # bits_v1 = 'channel-bits-events-v1.'
+    bits = 'channel-bits-events-v2.'
     bits_badge_notification = 'channel-bits-badge-unlocks.'
     channel_points = 'channel-points-channel-v1.'
     channel_subscriptions = 'channel-subscribe-events-v1.'
