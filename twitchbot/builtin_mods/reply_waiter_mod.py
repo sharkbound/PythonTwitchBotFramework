f4670b8aeae7f4439371690569b1b9a2647eb139
sharkbound <laptopblaster@gmail.com>
2020-09-30 22:20:56 -0500

    updated !gamble to award based on %, you dont only use how much the number was off of the total dice sides, payout increased as well

--- twitchbot/builtin_mods/reply_waiter_mod.py
+++ twitchbot/builtin_mods/reply_waiter_mod.py
@@ -3,6 +3,7 @@ from twitchbot import Mod, Message, reply_wait_queue, MessageType
 
 class ReplyWaiter(Mod):
     name = 'replywaiter'
+    TASK_NAME = ''
 
     async def on_raw_message(self, msg: Message):
         if not reply_wait_queue or msg.type not in {MessageType.WHISPER, MessageType.PRIVMSG}:
@@ -12,7 +13,11 @@ class ReplyWaiter(Mod):
         for i, (future, predicate) in enumerate(reply_wait_queue):
             if await predicate(msg):
                 # set the result of the future to the message, this makes the future complete and return its result
-                future.set_result(msg)
+                # FIXME: https://github.com/sharkbound/PythonTwitchBotFramework/issues/27
+                # bug cause comes from trying to set result on cancelled futures
+                if not future.cancelled():
+                    future.set_result(msg)
+                # need to move this to a thread/async save removal method
                 remove.append(i)
 
         for i in reversed(remove):
