2c6431131afa5b22da00b09ebf1df217736caa64
Allen Short <ashort@mozilla.com>
2021-01-13 19:56:49 -0600

    interaction

--- util/command_console_u.py
+++ util/command_console_u.py
@@ -2,21 +2,27 @@
 import asyncio
 
 import click
-from urwid import AsyncioEventLoop, Edit, Filler, Frame, MainLoop, Text
+from urwid import AsyncioEventLoop, Edit, Filler, Frame, MainLoop, Text, connect_signal
 
 loop = asyncio.get_event_loop()
+
 
 @click.command()
 @click.option('--host', prompt='Command server host', default='localhost')
 @click.option('--port', prompt='Command server port', default='1337')
 def run(host, port):
-    lines = ['example text']
+    lines = ['example text\n']
     output = Text(lines)
-    input = Edit('>> ')
-    widget = Frame(Filler(output, 'top'), footer=input)
+    input_field = Edit('>> ')
+    widget = Frame(Filler(output, 'top'), footer=input_field)
     widget.focus_position = 'footer'
     event_loop=AsyncioEventLoop(loop=loop)
-    MainLoop(widget, event_loop=event_loop).run()
+    def accept_input(key):
+        if key == 'enter':
+            lines.append(input_field.edit_text + '\n')
+            input_field.set_edit_text('')
+            output.set_text(lines)
+    MainLoop(widget, event_loop=event_loop, unhandled_input=accept_input).run()
 
 if __name__ == '__main__':
     run()
