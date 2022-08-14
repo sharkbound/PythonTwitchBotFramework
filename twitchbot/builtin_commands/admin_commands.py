ebbe0c53a6ba54bb315081a9dbb5d1b1a3de165e
sharkbound <laptopblaster@gmail.com>
2022-08-14 14:41:08 -0500

    added help translations for admin_commands.py and cmd_management_commands.py

--- twitchbot/builtin_commands/admin_commands.py
+++ twitchbot/builtin_commands/admin_commands.py
@@ -1,12 +1,12 @@
 import asyncio
 
-from twitchbot import Command, CommandContext, Message, get_bot, stop_all_tasks, translate
+from twitchbot import Command, CommandContext, Message, get_bot, stop_all_tasks, translate, create_translate_callable
 
 ADMIN_COMMAND_PERMISSION = 'admin'
 
 
 @Command('shutdown', aliases=['stop'], context=CommandContext.BOTH, permission=ADMIN_COMMAND_PERMISSION,
-         help='make the bot shutdown')
+         help=create_translate_callable('builtin_command_help_message_shutdown'))
 async def cmd_shutdown(msg: Message, *args):
     await msg.reply(translate('bot_shutting_down'))
     await get_bot().shutdown()
--- twitchbot/builtin_commands/cmd_management_commands.py
+++ twitchbot/builtin_commands/cmd_management_commands.py
@@ -10,6 +10,7 @@ from twitchbot import (
     channels,
     reload_whitelisted_commands,
     translate,
+    create_translate_callable,
 )
 
 MANAGE_COMMANDS_PERMISSION = 'manage_commands'
@@ -18,7 +19,7 @@ MANAGE_COMMANDS_PERMISSION = 'manage_commands'
 @Command('disablecmd',
          permission=MANAGE_COMMANDS_PERMISSION,
          syntax='<name>',
-         help='disables a command for the current channel')
+         help=create_translate_callable('builtin_command_help_message_disablecmd'))
 async def cmd_disable_cmd(msg: Message, *args):
     if not args:
         raise InvalidArgumentsError(reason=translate('missing_required_arguments'), cmd=cmd_disable_cmd)
@@ -40,7 +41,7 @@ async def cmd_disable_cmd(msg: Message, *args):
 @Command('enablecmd',
          permission=MANAGE_COMMANDS_PERMISSION,
          syntax='<name>',
-         help='enables a command for the current channel')
+         help=create_translate_callable('builtin_command_help_message_enablecmd'))
 async def cmd_enable_cmd(msg: Message, *args):
     if not args:
         raise InvalidArgumentsError(reason=translate('missing_required_arguments'), cmd=cmd_enable_cmd)
@@ -58,13 +59,13 @@ async def cmd_enable_cmd(msg: Message, *args):
     await msg.reply(translate('command_enabled', name=name))
 
 
-@Command('reloaddisabled', permission=MANAGE_COMMANDS_PERMISSION, help='reloads disable commands config')
+@Command('reloaddisabled', permission=MANAGE_COMMANDS_PERMISSION, help=create_translate_callable('builtin_command_help_message_reloaddisabled'))
 async def cmd_reload_disabled(msg: Message, *args):
     cfg_disabled_commands.load()
     await msg.reply(translate('reloaded_disabled_commands_config', user=msg.author))
 
 
-@Command('disablecmdglobal', permission=MANAGE_COMMANDS_PERMISSION, help='disables a command for all channels the bot is in')
+@Command('disablecmdglobal', permission=MANAGE_COMMANDS_PERMISSION, help=create_translate_callable('builtin_command_help_message_disablecmdglobal'))
 async def cmd_disable_command_global(msg: Message, *args):
     if not args:
         raise InvalidArgumentsError(reason=translate('missing_required_arguments'), cmd=cmd_disable_cmd)
@@ -83,7 +84,7 @@ async def cmd_disable_command_global(msg: Message, *args):
 @Command('enablecmdglobal',
          permission=MANAGE_COMMANDS_PERMISSION,
          syntax='<name>',
-         help='enables a command for all channels the bot is in')
+         help=create_translate_callable('builtin_command_help_message_enablecmdglobal'))
 async def cmd_enable_command_global(msg: Message, *args):
     if not args:
         raise InvalidArgumentsError(reason=translate('missing_required_arguments'), cmd=cmd_enable_cmd)
@@ -100,7 +101,7 @@ async def cmd_enable_command_global(msg: Message, *args):
 
 @Command('reloadcmdwhitelist',
          permission=MANAGE_COMMANDS_PERMISSION,
-         help='reloads whitelisted commands from the config file')
+         help=create_translate_callable('builtin_command_help_message_reloadcmdwhitelist'))
 async def cmd_reload_command_whitelist(msg: Message, *_):
     reload_whitelisted_commands()
     await msg.reply(translate('reloaded_command_whitelist', mention=msg.mention))
