0d9d194e34bd74a51bbd301228da7cac9e9fa376
sharkbound <laptopblaster@gmail.com>
2022-08-14 15:10:00 -0500

    added help translations for mod_management_commands.py

--- twitchbot/builtin_commands/mod_management_commands.py
+++ twitchbot/builtin_commands/mod_management_commands.py
@@ -10,13 +10,14 @@ from twitchbot import (
     mods,
     reload_mod,
     translate,
+    create_translate_callable,
 )
 
 MOD_MANAGE_PERMISSION = 'manage_mods'
 
 
 @Command('enablemod', context=CommandContext.CHANNEL, permission=MOD_MANAGE_PERMISSION,
-         syntax='<mod_name>', help='enables a mod for the current channel')
+         syntax='<mod_name>', help=create_translate_callable('builtin_command_help_message_enablemod'))
 async def cmd_enable_mod(msg: Message, *args):
     if len(args) != 1:
         raise InvalidArgumentsError(reason=translate('missing_required_arguments'), cmd=cmd_enable_mod)
@@ -34,7 +35,7 @@ async def cmd_enable_mod(msg: Message, *args):
 
 
 @Command('disablemod', context=CommandContext.CHANNEL, permission=MOD_MANAGE_PERMISSION,
-         syntax='<mod_name>', help='disables a mod for the current channel')
+         syntax='<mod_name>', help=create_translate_callable('builtin_command_help_message_disablemod'))
 async def cmd_disable_mod(msg: Message, *args):
     if len(args) != 1:
         raise InvalidArgumentsError(reason=translate('missing_required_arguments'), cmd=cmd_disable_mod)
@@ -51,7 +52,7 @@ async def cmd_disable_mod(msg: Message, *args):
     await msg.reply(translate('disablemod_disabled', mod=mod))
 
 
-@Command('disabledmods', context=CommandContext.CHANNEL, help='lists disabled mods for the current channel')
+@Command('disabledmods', context=CommandContext.CHANNEL, help=create_translate_callable('builtin_command_help_message_disabledmods'))
 async def cmd_disabled_mods(msg: Message, *args):
     await msg.reply(translate('disabledmods_list', channel_name=msg.channel_name) + ', '.join(
         mod for mod in mods if is_mod_disabled(msg.channel_name, mod)))
@@ -59,7 +60,7 @@ async def cmd_disabled_mods(msg: Message, *args):
 
 @Command('reloadmod', context=CommandContext.CHANNEL, syntax='<modname(case sensitive) or all>',
          permission=MOD_MANAGE_PERMISSION,
-         help='reloads a specific module (case sensitive) from disk')
+         help=create_translate_callable('builtin_command_help_message_reloadmod'))
 async def cmd_reload_mod(msg: Message, *args):
     if len(args) != 1:
         raise InvalidArgumentsError(translate('missing_required_arguments'), cmd=cmd_reload_mod)
@@ -82,7 +83,7 @@ async def cmd_reload_mod(msg: Message, *args):
         await msg.reply(translate('reloadmod_failed', mod=mod))
 
 
-@Command('listmods', context=CommandContext.CHANNEL, permission='listmods', help='reloads a specific module (case sensitive) from disk')
+@Command('listmods', context=CommandContext.CHANNEL, permission='listmods', help=create_translate_callable('builtin_command_help_message_listmods'))
 async def cmd_list_mods(msg: Message, *args):
     disabled_prefix = translate('listmods_disabled_prefix')
     mods_str = ', '.join(
