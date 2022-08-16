f5d28949c7a9821271504332e66c6bb7f6d7e064
sharkbound <laptopblaster@gmail.com>
2022-08-15 23:14:50 -0500

    generate_command_markdown.py now works with the bots lazy command loading on run() call

--- util/generate_command_markdown.py
+++ util/generate_command_markdown.py
@@ -1,7 +1,9 @@
-from twitchbot import commands
-from shutil import rmtree
 from pathlib import Path
+from twitchbot import commands, BaseBot
+from shutil import rmtree
 from os import remove
+
+BaseBot()._load_builtin_commands()
 
 with open('commands.md', 'w') as out:
     out.write('# Builtin Commands: \n')
@@ -28,6 +30,10 @@ HELP: {command.help or 'NO HELP'}
 # the bot creates these folders, and since we do not need them, just delete them
 CWD = Path.cwd()
 # noinspection PyTypeChecker
-remove(CWD / 'database.sqlite')
-rmtree(CWD / 'configs')
-rmtree(CWD / 'mods')
+
+if (sqlite_file := (CWD / 'database.sqlite')).exists():
+    remove(sqlite_file)
+if (configs_directory := (CWD / 'configs')).exists():
+    rmtree(configs_directory)
+if (mods_directory := (CWD / 'mods')).exists():
+    rmtree(mods_directory)
