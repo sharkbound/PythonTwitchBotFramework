17185c7a4fd94cbd310b35dafe8b2f064c43ece2
sharkbound <laptopblaster@gmail.com>
2022-08-31 12:29:11 -0500

    version 2.7.2

--- twitchbot/__init__.py
+++ twitchbot/__init__.py
@@ -1,6 +1,6 @@
 from .bot_package_path import _set_bot_package_path, get_bot_package_path
 
-BOT_VERSION = (2, 7, 1)
+BOT_VERSION = (2, 7, 2)
 _set_bot_package_path(__path__[0])
 
 from .arena import *
