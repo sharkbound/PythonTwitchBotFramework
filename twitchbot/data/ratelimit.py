989ee4dd5997c522b5bc30f9f9292e2a76ea633e
sharkbound <laptopblaster@gmail.com>
2022-11-22 14:01:03 -0600

    added error checking/handling in RateLimit.from_headers_or_none, renamed RateLimit.from_headers to RateLimit.from_headers_or_none

--- twitchbot/data/ratelimit.py
+++ twitchbot/data/ratelimit.py
@@ -11,7 +11,17 @@ class RateLimit(NamedTuple):
     reset: int
 
     @staticmethod
-    def from_headers(headers):
+    def from_headers_or_none(headers: dict):
+        """
+        :param headers: dict of headers from the twitch response, expects these keys to be present: ('Ratelimit-Limit', 'Ratelimit-Reset', 'Ratelimit-Remaining')
+        :return:
+            `Ratelimit` instance if all these keys are present ('Ratelimit-Limit', 'Ratelimit-Reset', 'Ratelimit-Remaining')
+            else returns None
+        """
+        for key in ('Ratelimit-Limit', 'Ratelimit-Reset', 'Ratelimit-Remaining'):
+            if key not in headers:
+                return None
+
         return RateLimit(
             limit=int(headers.get('Ratelimit-Limit')),
             remaining=int(headers.get('Ratelimit-Remaining')),
