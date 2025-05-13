49eeec0d720fb17fb185f1183a2e195dd4e19718
sharkbound <laptopblaster@gmail.com>
2025-05-13 16:10:45 -0500

    add notification parsing

--- twitchbot/eventsub/eventsub_message_parsers.py
+++ twitchbot/eventsub/eventsub_message_parsers.py
@@ -1,31 +1,48 @@
 import json
 from typing import Optional
 
-from .eventsub_message_types import EventSubMessage, EventSubWelcomeMessage, EventSubMessageType
+from .eventsub_message_types import (
+    EventSubMessage,
+    EventSubWelcomeMessage,
+    EventSubMessageType,
+    EventSubSessionReconnectMessage,
+    EventSubSessionKeepaliveMessage,
+    EventSubNotificationMessage,
+)
 
 
 def get_eventsub_message_type_str(json_data: dict) -> Optional[str]:
     if not isinstance(json_data, dict):
         return None
 
-    metadata = json_data.get('metadata', {})
+    metadata = json_data.get("metadata", {})
     if not isinstance(metadata, dict):
         return None
 
-    return metadata.get('message_type')
+    return metadata.get("message_type")
 
 
 def parse_eventsub_json(json_data: str) -> Optional[EventSubMessage]:
     try:
-        parsed = json.loads(json_data)  # todo: fix type error: "TypeError: the JSON object must be str, bytes or bytearray, not NoneType"
-    except json.JSONDecodeError:
+        parsed = json.loads(json_data)
+    except (json.JSONDecodeError, TypeError):
         return None
 
     message_type = get_eventsub_message_type_str(parsed)
     if message_type is None:
-        return EventSubMessage(parsed, message_type=EventSubMessageType.NOT_SET)
+        return EventSubMessage(parsed, message_type=EventSubMessageType.UNKNOWN)
 
     if message_type == "session_welcome":
         return EventSubWelcomeMessage.from_json(parsed)
 
-    return EventSubMessage(parsed, message_type=EventSubMessageType.NOT_SET)
+    if message_type == "session_keepalive":
+        return EventSubSessionKeepaliveMessage.from_json(parsed)
+
+    if message_type == "notification":
+        return EventSubNotificationMessage.from_json(parsed)
+
+    if message_type == "session_reconnect":
+        return EventSubSessionReconnectMessage.from_json(parsed)
+    # Left: Revocation
+
+    return EventSubMessage.from_json(parsed)
