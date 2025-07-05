51e9fb559f58538d510d0a03a1ad39165329b47d
sharkbound <laptopblaster@gmail.com>
2025-07-05 17:07:17 -0500

    Populate exports in eventsub init py file

--- twitchbot/eventsub/__init__.py
+++ twitchbot/eventsub/__init__.py
@@ -1,1 +1,13 @@
 from .eventsub_client import EventSubClient
+from .eventsub_topics import EventSubTopics
+from .eventsub_message_parsers import parse_eventsub_json, get_eventsub_message_type_str
+from .eventsub_message_types import (
+    EventSubMessage,
+    EventSubWelcomeMessage,
+    EventSubMessageType,
+    EventSubSessionReconnectMessage,
+    EventSubSessionKeepaliveMessage,
+    EventSubNotificationMessage,
+    EventSubRevocationMessage,
+    EventSubGenericMessage,
+)
--- twitchbot/eventsub/eventsub_message_types.py
+++ twitchbot/eventsub/eventsub_message_types.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from datetime import datetime
 from enum import Enum
-from typing import Optional, Union, TYPE_CHECKING
+from typing import Optional, Union
 
 def parse_twitch_timestamp(timestamp: str) -> datetime:
     # Remove the trailing 'Z' if it exists
