8f02cd5421c591777b4fe3ad001f5d7c182783e3
sharkbound <laptopblaster@gmail.com>
2025-08-15 15:50:30 -0500

    update url_generator_helper.py

--- url_generator_helper.py
+++ url_generator_helper.py
@@ -8,10 +8,9 @@ with open('configs/config.json', 'r') as f:
 pubsub_url = token_utils.generate_auth_url(
     secrets["client_id"],
     'https://twitchapps.com/tmi/',
-    Scopes.PUBSUB_CHANNEL_POINTS,
-    Scopes.PUBSUB_BITS,
-    Scopes.PUBSUB_CHANNEL_SUBSCRIPTION,
-    Scopes.PUBSUB_MODERATOR_ACTIONS
+    'moderator:read:blocked_terms', 'moderator:read:chat_settings', 'moderator:read:unban_requests',
+    'moderator:read:banned_users', 'moderator:read:chat_messages', 'moderator:read:warnings', 'moderator:read:vips',
+    'moderator:read:moderators',
 )
 print(f'OAUTH URL: {token_utils.generate_irc_oauth(secrets["client_id"], "https://twitchapps.com/tmi/")}')
 print(f'EVENTSUB: {pubsub_url}')
